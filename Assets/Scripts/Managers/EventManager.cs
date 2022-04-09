32e46d53c1857f71b00e1d860ec61702e86a83eb
Friar Hob <github@friarhob.33mail.com>
2022-04-09 19:25:29 +0100

    Refactoring - declaring basic managers

--- Assets/Scripts/Managers/EventManager.cs
+++ Assets/Scripts/Managers/EventManager.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class EventManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    public static EventManager Instance;
 
     void Awake()
     {
